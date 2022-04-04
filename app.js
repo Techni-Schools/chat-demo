73f51acb2c2674371ac11922558cad3e6d395720
MTaborowski-TS <96332103+MTaborowski-TS@users.noreply.github.com>
2022-04-04 10:05:12 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,112 @@
+const express = require('express')
+const session = require('express-session')
+const ws = require('ws');
+const http = require('http');
+const _ = require('lodash');
+const app = express()
+const users = [];
+let i = 0;
+
+app.use(express.static('public'))
+const sessionParser = session({
+  secret: 'secureToken',
+  resave: false,
+  saveUninitialized: false
+});
+
+app.use(sessionParser);
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+const server = http.createServer(app);
+const wss = new ws.WebSocket.Server({clientTracking: true, noServer: true});
+
+server.on('upgrade', function(request, socket, head) {
+
+  sessionParser(request, {}, () => {
+    if (!request.session.user) {
+      socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n');
+      socket.destroy();
+      return;
+    }
+
+    if (request.url === '/chat') {
+      wss.handleUpgrade(request, socket, head, function(ws) {
+        wss.emit('connection', ws, request);
+      });
+    }
+  });
+});
+
+app.get('/test', (req, res) => {
+  res.send('hello world')
+})
+
+app.post('/user/register', (req, res) => {
+  if (users[req.body.user_name]) {
+    res.status(409).send('User already exists');
+  } else {
+    req.session.user = req.body.user_name
+    users[req.session.user] = {chats: [], name: req.session.user};
+
+    res.redirect(301, '/chat.html')
+  }
+})
+
+app.get('/user/list', (req, res) => {
+  res.json(_.compact(_.values(users)));
+})
+
+app.get('/user', (req, res) => {
+  const user = req.session.user
+  if (req.session.user) {
+    res.json({name: user, chats: users[user].chats});
+  } else {
+    res.sendStatus(401)
+  }
+})
+
+app.post('/user/chat-with', (req, res) => {
+  users[req.session.user].chats.push(req.body.user);
+  const chatId = i++;
+  wss.clients.forEach(client => {
+    if (client.user === req.body.user) {
+      client.send(JSON.stringify({type: 'USER.CHAT', data: {from: req.session.user, to: client.user, id: chatId}}))
+    }
+  });
+  res.json({to: req.session.user, from: req.body.user, id: chatId});
+})
+
+wss.on('connection', function(ws, req) {
+  ws.user = req.session.user;
+
+  wss.clients.forEach(client => {
+    if (client !== ws) {
+      client.send(JSON.stringify({type: 'USER.JOIN', data: req.session.user}))
+    }
+  });
+
+  ws.on('close', () => {
+    delete users[ws.user];
+    wss.clients.forEach(client => {
+      if (client !== ws) {
+        client.send(JSON.stringify({type: 'USER.LEFT', data: req.session.user}))
+      }
+    });
+  })
+
+  ws.on('message', function(msg) {
+    const message = JSON.parse(msg);
+    if (message.type === 'CHAT.MESSAGE' || message.type === 'CHAT.DECLINE' || message.type === 'CHAT.END') {
+      wss.clients.forEach(client => {
+        if (client.user === message.data.to) {
+          client.send(JSON.stringify({type: message.type, data: message.data}))
+        }
+      });
+    }
+  });
+});
+
+server.listen(3000, function() {
+  console.log('Listening on http://localhost:3000');
+});
---
+++ public/script.js
@@ -0,0 +1,162 @@
+class Application {
+  constructor() {
+    this.user = {chats: {}};
+    this.chat = new Chat(this.user);
+  }
+}
+
+class Chat {
+  constructor(user) {
+    this.user = user;
+  }
+
+  async init() {
+    await this.getMe();
+    await this.loadUsers();
+    this.openWebSocket();
+  }
+
+  async loadUsers() {
+    return fetch('/user/list')
+        .then(response => {
+          return response.json();
+        })
+        .then(response => {
+          document.getElementById('user_list').innerHTML = _.reduce(response, (list, el) => {
+            if (el.name !== this.user.name) {
+              return list += `<li class='list-group-item'><span class="label">${el.name}</span> <button class="btn text-primary" type="button" onclick="app.chat.initiateChatWith('${el.name}')"><i class="bi bi-chat"></i></button></li>`;
+            } else {
+              return list;
+            }
+          }, '')
+        });
+  }
+
+  async getMe() {
+    return fetch('/user')
+        .then(response => {
+          if (response.status !== 200) {
+            throw {status: response.status, statusText: response.statusText};
+          }
+          return response.json();
+        })
+        .then(response => {
+          this.user.name = response.name;
+          document.getElementById("me").innerText = this.user.name;
+        })
+        .catch(reason => {
+          if (reason.status === 401) {
+            window.location = '/users.html';
+          } else if (reason.status === 409) {
+            alert(reason.statusText);
+          }
+        })
+  }
+
+  initiateChatWith(user) {
+    return fetch('http://localhost:3000/user/chat-with', {
+      method: 'POST', headers: {
+        'Content-Type': 'application/json'
+      }, body: JSON.stringify({user})
+    }).then(response => {
+      return response.json();
+    }).then(response => {
+      this.startChat(response);
+    })
+  }
+
+  openWebSocket() {
+    this.ws = new WebSocket('ws://localhost:3000/chat')
+
+    this.ws.onopen = () => {
+      console.log('ws opened');
+    }
+
+    this.ws.onmessage = (message) => {
+      const msg = JSON.parse(message.data);
+      if (msg.type === 'USER.JOIN' || msg.type === 'USER.LEFT') {
+        this.loadUsers();
+      }
+
+      if (msg.type === 'USER.CHAT') {
+        if (confirm(`Do you want to chat with ${msg.data.from} ?`)) {
+          this.startChat(msg.data);
+        } else {
+          this.declineChat(msg.data);
+        }
+      }
+
+      if (msg.type === 'CHAT.MESSAGE') {
+        this.addMessageToChat(msg.data);
+      }
+      if (msg.type === 'CHAT.DECLINE' || msg.type === 'CHAT.END') {
+        this.addSystemMessageToChat(msg.data);
+      }
+    }
+  }
+
+  startChat(data) {
+    this.user.chats[data.id] = data.from;
+    document.getElementById("chats-container").insertAdjacentHTML('beforeend', this.getNewChatWindow(data));
+  }
+
+  getNewChatWindow(data) {
+    return `
+    <div class="chat-window card shadow" id="chat-${data.id}" xmlns="http://www.w3.org/1999/html">
+      <div class="card-header"><span class="chat-title">${data.from}</span><button type="button" class="btn" onclick="app.chat.closeChat(${data.id})"><i class="bi bi-x-lg"></i></button> </div>
+      <div class="card-body" >
+        <ul class="chat-messages p-1 mb-3 rounded shadow-sm">
+      
+        </ul>
+        <div class="chat-controls">
+          <input type="text" id="chat-input-${data.id}" class="form-control" onkeyup="app.chat.enter(event,${data.id})"/>
+          <button type="button" id="chat-send-btn-${data.id}" onclick="app.chat.sendMessage(${data.id})" class="btn"><i class="bi bi-send"></i></button>
+        </div>
+      </div>
+    </div>
+    `;
+  }
+
+  enter(event, chatId) {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      document.getElementById(`chat-send-btn-${chatId}`).click();
+    }
+  }
+
+  sendMessage(chatId) {
+    const text = document.getElementById('chat-input-' + chatId).value;
+
+    this.ws.send(JSON.stringify({
+      type: 'CHAT.MESSAGE',
+      data: {text: text, to: this.user.chats[chatId], from: this.user.name, id: chatId}
+    }));
+    document.querySelector(`#chat-${chatId} .chat-messages`).insertAdjacentHTML('beforeend', `<li class="msg msg-sent">${text}</li>`)
+    document.getElementById('chat-input-' + chatId).value = '';
+  }
+
+  addMessageToChat(data) {
+    document.querySelector(`#chat-${data.id} .chat-messages`).insertAdjacentHTML('beforeend', `<li class="msg msg-received">${data.text}</li>`)
+  }
+
+  addSystemMessageToChat(data) {
+    document.querySelector(`#chat-${data.id} .chat-messages`).insertAdjacentHTML('beforeend', `<li class="msg msg-system">${data.text}</li>`)
+  }
+
+  declineChat(data) {
+    this.ws.send(JSON.stringify({
+      type: 'CHAT.DECLINE',
+      data: {text: 'User declined chat', to: data.from, from: this.user.name, id: data.id}
+    }));
+  }
+
+  closeChat(chatId) {
+    this.ws.send(JSON.stringify({
+      type: 'CHAT.END',
+      data: {text: 'User closed the chat', to: this.user.chats[chatId], from: this.user.name, id: chatId}
+    }));
+    document.querySelector(`#chat-${chatId}`).remove();
+  }
+}
+
+const app = new Application();
